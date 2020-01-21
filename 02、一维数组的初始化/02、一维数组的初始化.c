866091fd7769be1262669e32604fdd6004ea71c6
BitBitcode <1378028451@qq.com>
2020-01-21 21:30:30 +0800

    今天就学到这里吧

---
+++ 02、一维数组的初始化/02、一维数组的初始化.c
@@ -0,0 +1,43 @@
+﻿//《C语言从入门到精通》P125
+// 第 5 章 数  组
+// 5.1 一维数组
+// 02、一维数组的初始化
+
+#include<stdio.h>
+
+int main()
+{
+	int i;
+	int j;
+	
+	// 全部初始化
+	int a[5] = { 1, 2, 3, 4, 5 };
+	printf("全部初始化之后的结果：\n");
+	for (i = 0; i < 5; i++)
+	{
+		printf("a[%d] = %d \n", i, a[i]);
+	}
+	printf("\n");
+
+	// 部分初始化
+	int b[5] = { 1, 2, 3 };
+	printf("部分初始化之后的结果：\n");
+	for (j = 0; j < 5; j++)
+	{
+		printf("b[%d] = %d \n", j, b[j]);
+	}
+	printf("\n");
+
+	// 小技巧：隔位输出
+	printf("隔 1 位输出一个元素（计数变量加 2 即可）\n");
+	for (i = 0; i < 5; i += 2)
+	{
+		printf("a[%d] = %d \n", i, a[i]);
+	}
+	printf("\n");
+	printf("隔 2 位输出一个元素（计数变量加 3 即可）\n");
+	for (j = 0; j < 5; j += 3)
+	{
+		printf("b[%d] = %d \n", j, b[j]);
+	}
+}
---
+++ 04、选票统计/04、选票统计.c
@@ -0,0 +1,56 @@
+﻿//《C语言从入门到精通》P125
+// 第 5 章 数  组
+// 5.1 一维数组
+// 04、选票统计
+
+#include<stdio.h>
+
+int main()
+{
+	int i;
+	int p;					// 参加选举投票的人数
+	int a[20] = {0};		// 记录选票（由于不能以变量作为数组大小，只能按一个较大的数来粗略定义）
+	int vote_Kiana = 0;		// 琪亚娜的得票数
+	int vote_Mei = 0;		// 芽衣的得票数
+	int vote_Bronya = 0;	// 布洛妮娅的得票数
+	int vote_invalid = 0;	// 无效票数
+
+	printf("请输入参加投票的人数（20以内）：\n");
+	scanf_s("%d", &p);
+
+	printf("请输入投票结果（输入对应编号，按回车继续）：\n 1、琪亚娜；\n 2、芽衣；\n 3、布洛妮娅；\n 0、弃  权\n");
+	for (i = 0; i < p; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+
+	// 统计得票情况
+	for (i = 0; i < p; i++)
+	{
+		if (a[i] == 1)
+		{
+			vote_Kiana ++;
+		}
+
+		else if (a[i] == 2)
+		{
+			vote_Mei ++;
+		}
+
+		else if (a[i] == 3)
+		{
+			vote_Bronya ++;
+		}
+
+		else	// 包括“弃  权”票和投票结果不是“1”、“2”、“3”的无效情况
+		{
+			vote_invalid ++;
+		}
+	}
+
+	// 宣布选举结果
+	printf("\n琪亚娜的得票数为：%d \n", vote_Kiana);
+	printf("芽衣的得票数为：%d \n", vote_Mei);
+	printf("布洛妮娅的得票数为：%d \n", vote_Bronya);
+	printf("无效票数为：%d \n", vote_invalid);
+}
