cac5681374d6c75e44a6237158bfbb2835e1a660
BitBitcode <1378028451@qq.com>
2020-01-30 00:57:43 +0800

    +

--- 14、字符串处理函数（strlwr、strupr）/14、字符串处理函数（strlwr、strupr）.c
+++ 14、字符串处理函数（strlwr、strupr）/14、字符串处理函数（strlwr、strupr）.c
@@ -8,6 +8,32 @@
 
 int main()
 {
+	char str1[10];
+	char str2[10];
+
+	// strlwr()将大写字母转换为小写字母，其他字母不变
+	printf("请输入一些字母，同时包含大写字母和小写字母：\n");
+	gets_s(str1, 10);
+	printf("已输入：\n");
+	puts(str1);
+	printf("\n");
+
+	printf("strlwr()函数将大写字母转换为小写字母：\n");
+	_strlwr_s(str1, 10);	// _strlwr_s()函数有变化，要给出字符串的长度
+	puts(str1);
+
+	printf("\n");
+
+	// strlwr()将小写字母转换为大写字母，其他字母不变
+	printf("请输入一些字母，同时包含大写字母和小写字母：\n");
+	gets_s(str2, 10);
+	printf("已输入：\n");
+	puts(str2);
+	printf("\n");
+
+	printf("strupr()函数将小写字母转换为大写字母：\n");	
+	_strupr_s(str2, 10);	// _strupr_s()函数有变化，要给出字符串的长度
+	puts(str2);
 
 	return 0;
 }
---
+++ 15、统计各种字符的个数/15、统计各种字符的个数.c
@@ -0,0 +1,47 @@
+﻿//《C语言从入门到精通》P148
+// 第 5 章 数  组
+// 5.3 字符数组
+// 15、统计各种字符的个数
+
+// 复习：
+// ASCII码值在48~57之间：数字
+// ASCII码值在65~90：大写字母
+// ASCII码值在97~122：小写字母
+// 除此之外是特殊字符
+
+#include<stdio.h>
+
+int main()
+{
+	char c;				// 字符变量
+	int letters = 0;	// 字母的个数
+	int spaces = 0;		// 空格的个数
+	int digit = 0;		// 数字的个数
+	int others = 0;		// 其他
+	
+	printf("请输入一个字符串：\n");
+	//c = getch();
+	while ((c = getchar) != '\n')
+	{
+		if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		{
+			letters++;	// 统计字母
+		}
+		else if (c == ' ')
+		{
+			spaces++;	// 统计空格
+		}
+		else if (c >= '0' && c <= '9')
+		{
+			digit++;	// 统计数字
+		}
+		else
+		{
+			others++;	// 统计其他字符
+		}
+	}
+
+	printf("字母：%3d \n空格：%3d \n数字：%3d \n其他：%3d \n", letters, spaces, digit, others);
+
+	return 0;
+}
