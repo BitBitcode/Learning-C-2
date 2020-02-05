974eb82fe886f0a9153b920398734fdddbb2e50c
BitBitcode <1378028451@qq.com>
2020-02-05 12:35:24 +0800

    学习学习

--- 21、计算任意两个整数的积/21、计算任意两个整数的积.c
+++ 21、计算任意两个整数的积/21、计算任意两个整数的积.c
@@ -2,3 +2,38 @@
 // 第 6 章 函  数
 // 6.1 函数的简介
 // 21、计算任意两个整数的积
+
+#include<stdio.h>
+
+
+//【声明】
+float multiplication(float num_1, float num_2);
+
+
+int main()
+{
+	float a, b;	// 两个因数
+	float c;		// 两数的积
+
+	printf("请输入第一个数：\n");
+	scanf_s("%f", &a);
+	printf("请输入第二个数：\n");
+	scanf_s("%f", &b);
+
+	c = multiplication(a, b);	// 【调用】
+
+	printf("两个数的乘积为：%f\n", c);
+
+	return 0;
+}
+
+
+//【定义】
+float multiplication(float num_1, float num_2)
+{
+	float result;
+
+	result = num_1 * num_2;
+
+	return result;
+}
---
+++ 22、求最大公约数与最小公倍数/22、求最大公约数与最小公倍数.c
@@ -0,0 +1,69 @@
+﻿//《C语言从入门到精通》P162
+// 第 6 章 函  数
+// 6.1 函数的简介
+// 22、求最大公约数与最小公倍数
+
+#include<stdio.h>
+
+
+//【声明】
+int max_common_divisor(int num_1, int num_2);		//【函数】求最大公约数
+int min_common_multiple(int num_1, int num_2);		//【函数】求最小公倍数
+
+
+//【主函数】
+int main()
+{
+	int a, b;
+	int x, y;
+
+	printf("请输入要求的两个数：\n");
+	printf("第 1 个数：");
+	scanf_s("%d", &a);
+	printf("\n");
+	printf("第 2 个数：");
+	scanf_s("%d", &b);
+	printf("\n");
+	
+	x = max_common_divisor(a, b);	//【调用】
+	y = min_common_multiple(a, b);	//【调用】
+
+	printf("两个数的最大公约数为：%d\n", x);
+	printf("两个数的最小公倍数为：%d\n", y);
+
+	return 0;
+}
+
+
+//【定义】
+int max_common_divisor(int num_1, int num_2)		//【函数】求最大公约数
+{
+	int t, r;
+
+	
+	if (num_1 < num_2)
+	{
+		t = num_2;
+		num_2 = num_1;
+		num_1 = t;
+	}
+
+	while ((r = num_1 % num_2) != 0)
+	{
+		num_1 = num_2;
+		num_2 = r;
+	}
+
+	return num_2;
+}
+
+
+int min_common_multiple(int num_1, int num_2)		//【函数】求最小公倍数
+{
+	int h;
+
+	h = max_common_divisor(num_1, num_2);
+
+	return (num_1 * num_2 / h);		
+	// return 语句其实是有括号的，括号内可以是表达式，而平时只有一个数就省略了
+}
