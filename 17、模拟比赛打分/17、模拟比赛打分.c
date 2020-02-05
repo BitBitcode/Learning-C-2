0c208edb99d7bbd3183b2317840468a2882a3cf6
BitBitcode <1378028451@qq.com>
2020-02-05 10:45:46 +0800

    学习

---
+++ 17、模拟比赛打分/17、模拟比赛打分.c
@@ -0,0 +1,58 @@
+﻿//《C语言从入门到精通》P150
+// 第 5 章 数  组
+// 5.5 经典范例
+// 17、模拟比赛打分
+
+#include<stdio.h>
+//#include<string.h>
+
+
+int main()
+{
+	int i;
+	int j;
+	int n;
+	float a[100];
+	float b[100];
+	float sum = 0;
+
+	printf("请输入选手人数：\n");
+	scanf_s("%d", &n);
+	
+	j = 1;
+	for (i = 1; i <= n; i++)
+	{
+		printf("选手%d \n", i);
+		printf("请输入裁判所给分数：\n");
+		for (; j < 5 * n+1; j++)
+		{
+			scanf_s("%f", &a[j]);
+			sum += a[j];
+			if (j % 5 == 0)
+			{
+				break;
+			}
+		}
+		b[i] = sum;
+		sum = 0;
+		j++;
+	}
+	j = 1;
+	printf("选手 \t裁判A \t裁判B \t裁判C \t裁判D \t裁判E \t总分 \n");
+	for (i = 1; i <= n; i++)
+	{
+		printf("\n选手%d \n", i);
+		for (; j <= 5 * n+1; j++)
+		{
+			printf("\t%3.1f", a[j]);
+			if (j % 5 == 0)
+			{
+				break;
+			}
+		}
+		printf("\t%3.2f \n", b[i]);
+		j++;
+	}
+
+	return 0;
+}
---
+++ 20、函数的声明、定义与调用/20、函数的声明、定义与调用.c
@@ -0,0 +1,41 @@
+﻿//《C语言从入门到精通》P156
+// 第 6 章 函  数
+// 6.1 函数的简介
+// 20、函数的声明、定义与调用
+
+
+#include<stdio.h>
+#include"function_paint.h"
+
+// 函数的声明（结尾需要加上分号）
+void move();
+void build();
+void worker();
+
+
+// 主函数部分（主函数可以调用其他函数，但其他函数不能调用主函数）
+int main()
+{
+	move();		// 定义在C文件中的函数不需要写“#inclde"XXX.c”的预处理语句”
+	printf("\n");
+	build();	// 调用函数：build()
+	printf("\n");
+	paint();	// 定义在C文件中的函数不需要写“#inclde"XXX.c”的预处理语句”
+	printf("\n");
+	
+	// 注意：不能嵌套函数定义语句，即定义不能放在函数体中
+	return 0;
+}
+
+
+// 函数的定义（结尾不加分号）
+void build()
+{
+	worker();		// 函数可以调用其他函数
+	printf("执行建造功能 \n");
+}
+
+void worker()
+{
+	printf("召集工人 \n");
+}
