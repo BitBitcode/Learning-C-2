c3113efed707fd4ac308e8a2b2e247f5f67bb234
BitBitcode <1378028451@qq.com>
2020-02-06 22:04:10 +0800

    修复杨辉三角BUG
    课本的杨辉三角例子的代码有误！

--- 07、打印杨辉三角/07、打印杨辉三角.c
+++ 07、打印杨辉三角/07、打印杨辉三角.c
@@ -4,32 +4,42 @@
 // 07、打印杨辉三角
 
 #include<stdio.h>
+#define LINE 9			// 宏定义：打印的行数
 
 int main()
 {
 	int i;
 	int j;
-	int a[11][11];
+	int a[LINE][LINE] = { 0 };	// 初始化
 	
-	for (i = 0; i < 11; i++)
+	// 首列与对角线元素
+	for (i = 0; i < LINE; i++)
 	{
-		a[i][i] = 1;
-		a[i][0] = 1;
+		a[i][i] = 1;	// 对角线元素全为 1
+		a[i][0] = 1;	// 第一列元素全为 1
 	}
 
-	for (i = 3; i < 11; i++)
+	// 其余元素
+	for (i = 2; i < LINE; i++)
 	{
-		for (j = 2; j < i-1; j++)
+		for (j = 1; j < i; j++)
 		{
 			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
 		}
 	}
 
-	for (i = 0; i < 11; i++)
+	// 输出
+	for (i = 0; i < LINE; i++)
 	{
-		for (j = 0; j < 11; j++)
+
+		for (int k = 1; k < (LINE - i); k++)
 		{
-			printf("%d，", a[i][j]);
+			printf("  ");			// 左边打印空格，使原本的直角三角形打印为等腰三角形
+		}
+
+		for (j = 0; j < i+1; j++)	// 每行的元素个数刚好等于行下标加 1（即第n行有n个元素）
+		{
+			printf("%3d ", a[i][j]);
 		}
 		printf("\n");
 	}
