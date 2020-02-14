c2b5a8f1deb05975fbd0e364216e4b7f75be71a5
BitBitcode <1378028451@qq.com>
2020-02-14 13:01:03 +0800

    学习

---
+++ 42、用指针求最大最小值/42、用指针求最大最小值.c
@@ -0,0 +1,51 @@
+﻿//《C语言从入门到精通》P230
+// 第 7 章 指  针
+// 7.2 一维数组与指针
+// 42、用指针求最大最小值
+
+#include<stdio.h>
+
+
+//【声明】
+void max_min(int a[], int n, int* max, int* min);
+
+
+//【主函数】
+int main()
+{
+	int i, A[10];
+	int max_A, min_A;								// max_a、min_a 是变量
+
+	printf("请输入：\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("A[%d] = ", i);
+		scanf_s("%d", &A[i]);
+	}
+
+	max_min(A, 10, &max_A, &min_A);
+
+	printf("最大值为：%d \n", max_A);
+	printf("最小值为：%d \n", min_A);
+
+	return 0;
+}
+
+
+//【定义】
+void max_min(int a[], int n, int* max, int* min)	// max、min 是指针
+{
+	int* p;
+	*max = *min = a[0];		// 或：*max = *min = *a
+	for (p = a + 1; p < a + n; p++)
+	{
+		if (*p > * max)
+		{
+			*max = *p;
+		}
+		else if (*p < *min)
+		{
+			*min = *p;
+		}
+	}
+}
---
+++ 43、逆序存放数组元素/43、逆序存放数组元素.c
@@ -0,0 +1,56 @@
+﻿//《C语言从入门到精通》P231
+// 第 7 章 指  针
+// 7.2 一维数组与指针
+// 43、逆序存放数组元素
+
+#include<stdio.h>
+
+
+//【声明】
+int invert(int* x, int n);
+
+
+//【主函数】
+int main()
+{
+	int i;
+	int a[10] = { 1,2,3,4,5,6,7,8,9,0 };
+
+	printf("原数组：");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\t", a[i]);
+	}
+	printf("\n");
+
+	invert(a, 10);			//【调用】
+
+	printf("现数组：");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\t", a[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
+
+
+//【定义】
+int invert(int* x, int n)
+{
+	int* p, * i, * j;
+	int temp, m;
+
+	m = (n - 1) / 2;
+	i = x;				// 数组首地址
+	j = x + n - 1;		// 数组末地址
+	p = x + m;			// 数组中间元素地址
+
+	for (; i <= p; i++, j--)	// 左边往右走，右边往左走
+	{
+		temp = *i;
+		*i = *j;
+		*j = temp;
+	}
+}
