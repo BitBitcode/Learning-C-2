172124c28be324840d13802cb7900d35ca9e725d
BitBitcode <1378028451@qq.com>
2020-02-15 17:59:52 +0800

    +

--- 44、二维数组与指针/44、二维数组与指针.c
+++ 44、二维数组与指针/44、二维数组与指针.c
@@ -8,64 +8,38 @@
 int main()
 {
 	//【定义二维数组的指针】
-	int A[3][5] = { 0,1,2,3,4 }, i, j;
-	int B[3][5] = { 9,8,7,6,5 }, k, l;
-	int* p, * q;
+	int A[3][5], i, j, * p;
+	int B[3][5], k, l, * q;
 	
 	p = A[0];		// A[0]代表第 0 行的首地址，即：&A[0][0]
 	q = &B[0][0];	// 以上两种写法是等价的
 
-	//printf("\n【使用指针输入二维数组】\n");
-	//printf("请输入 A 数组：\n");
-	//for (i = 0; i < 3; i++)
-	//{
-	//	for (j = 0; j < 5; j++)
-	//	{
-	//		printf("A[%d][%d] = ", i, j);
-	//		scanf_s("%d", (A[i] + j));	// 通过数组名访问地址
-	//	}
-	//	printf("\n");
-	//}
-	//printf("\n");
-
-	//printf("请输入 B 数组：\n");
-	//for (k = 0; k < 3; k++)
-	//{
-	//	for (l = 0; l < 5; l++)
-	//	{
-	//		printf("B[%d][%d] = ", k, l);
-	//		scanf_s("%d", q++);			// 通过指针访问地址
-	//	}
-	//	printf("\n");
-	//}
-	//printf("\n");
-
-
-	printf("\n【使用指针输出某一指定元素】\n");
-	printf("请输入想要显示的 A 数组元素下标：\n");
-	printf("i = ");
-	scanf_s("%d", &i);
-	printf("j = ");
-	scanf_s("%d", &j);
-	printf("A[%d][%d] = %d \n", i, j, *(A[i]+j));			// 通过数组名调用地址
-
-	q = B[0];												// 【注意】调用指针输出前一定要先使指针回位！
-	printf("请输入想要显示的 B 数组元素下标：\n");
-	printf("k = ");
-	scanf_s("%d", &k);
-	printf("l = ");
-	scanf_s("%d", &l);
-	printf("B[%d][%d] = %d \n", k, l, *((q+k)+l));			// 通过指针调用地址
+	printf("【使用指针输入二维数组】\n");
+	printf("请输入 A 数组：\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("A[%d][%d] = ", i, j);
+			scanf_s("%d", (A[i] + j));	// 通过数组名访问地址
+		}
+		printf("\n");
+	}
 	printf("\n");
 
+	printf("请输入 B 数组：\n");
+	for (k = 0; k < 3; k++)
+	{
+		for (l = 0; l < 5; l++)
+		{
+			printf("B[%d][%d] = ", k, l);
+			scanf_s("%d", q++);			// 通过指针访问地址
+		}
+		printf("\n");
+	}
+	printf("\n");
 
-	printf("\n【使用指针输出某一行、列】\n");
-
-
-
-
-
-	printf("\n【使用指针输出整个二维数组】\n");
+	printf("【使用指针输出整个二维数组】\n");
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 5; j++)
@@ -87,10 +61,6 @@ int main()
 		printf("\n");
 	}
 	printf("\n");
-
-	printf("\n【使用指针输出某一行、某一列】\n");
-		
-
 
 	return 0;
 }
