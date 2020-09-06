570d8fab20d0d9490892c7c7707c2ad071e67435
Acrylic-Studio <smilewwc@qq.com>
2020-09-06 19:50:23 +0800

    keep learning

---
+++ 64、链表/64、链表.c
@@ -0,0 +1,35 @@
+﻿//《C语言从入门到精通》P290
+// 第 8 章 结构体与共用体
+// 8.5 链  表
+// 64、链表
+
+
+#include<stdio.h>
+
+
+struct Student
+{
+	char name[20];
+	int num;
+	float score;
+	struct Student* pNext;
+};
+
+
+int i;
+
+struct Student* create()
+{
+	struct Student* pHead = NULL;
+	struct Student* pEnd;
+	struct Student* pNew;
+	i = 0;
+}
+
+
+int main()
+{
+	
+
+	return 0;
+}
